dbcffc6a138c7ff9c7ec1e06e0ba81866f4bcc28
Matthew Daly <matthewbdaly@gmail.com>
2018-01-05 12:03:24 +0000

    Added some comments

--- src/App.js
+++ src/App.js
@@ -17,10 +17,15 @@ class ButtonPage extends Component {
   render() {
     return (
       <div>
+        <p>The default button has a white background</p>
         <Button>Default Button</Button>
+        <p>The <code>main</code> theme turns it blue</p>
         <Button theme="main">Button with the main theme</Button>
+        <p>The <code>positive</code> theme turns it green</p>
         <Button theme="positive">Button with the positive theme</Button>
+        <p>The <code>negative</code> theme turns it red</p>
         <Button theme="negative">Button with the negative theme</Button>
+        <p>The <code>inverse</code> property inverts the theme</p>
         <Button theme="main" inverse="true">Button with the inverse main theme</Button>
         <Button theme="positive" inverse="true">Button with the inverse positive theme</Button>
         <Button theme="negative" inverse="true">Button with the inverse negative theme</Button>
@@ -33,7 +38,9 @@ class CardPage extends Component {
   render() {
     return (
       <div>
+        <p>Cards are simple containers for your content. They include a header field.</p>
         <Card header="Default card">This is an example of a default Card component</Card>
+        <p>Cards can contain listviews.</p>
         <Card header="Card with list view inside">
           <ListView>
             <ListViewItem>Item 1</ListViewItem>
@@ -49,12 +56,15 @@ class CardPage extends Component {
 class TabBarPage extends Component {
   render() {
     return (
-      <TabBar>
-        <TabBarItem active="true">Tab 1</TabBarItem>
-        <TabBarItem>Tab 2</TabBarItem>
-        <TabBarItem>Tab 3</TabBarItem>
-        <TabBarItem>Tab 4</TabBarItem>
-      </TabBar>
+      <div>
+        <p>Tab bars are bars that can contain multiple <code>TabBarItem</code> components. They use Flexbox so you should be able to add as many as you need and as long as you don't go overboard they should Just Work.</p>
+        <TabBar>
+          <TabBarItem active="true">Tab 1</TabBarItem>
+          <TabBarItem>Tab 2</TabBarItem>
+          <TabBarItem>Tab 3</TabBarItem>
+          <TabBarItem>Tab 4</TabBarItem>
+        </TabBar>
+      </div>
     );
   }
 }
@@ -62,32 +72,36 @@ class TabBarPage extends Component {
 class ListViewPage extends Component {
   render() {
     return (
-      <ListView>
-        <ListViewItem>Item 1</ListViewItem>
-        <ListViewItem>Item 2</ListViewItem>
-        <ListViewItem>Item 3</ListViewItem>
-        <ListViewItem>
-          <Badge>5</Badge>
-        </ListViewItem>
-        <ListViewItem>
-          <Badge theme="main">5</Badge>
-        </ListViewItem>
-        <ListViewItem>
-          <Badge theme="positive">5</Badge>
-        </ListViewItem>
-        <ListViewItem>
-          <Badge theme="negative">5</Badge>
-        </ListViewItem>
-        <ListViewItem>
-          <Badge theme="main" inverse="true">5</Badge>
-        </ListViewItem>
-        <ListViewItem>
-          <Badge theme="positive" inverse="true">5</Badge>
-        </ListViewItem>
-        <ListViewItem>
-          <Badge theme="negative" inverse="true">5</Badge>
-        </ListViewItem>
-      </ListView>
+      <div>
+        <p>List views are formatted lists of <code>ListViewItem</code> components, which themselves can contain <code>Badge</code> components.</p>
+
+        <ListView>
+          <ListViewItem>Item 1</ListViewItem>
+          <ListViewItem>Item 2</ListViewItem>
+          <ListViewItem>Item 3</ListViewItem>
+          <ListViewItem>
+            <Badge>5</Badge>
+          </ListViewItem>
+          <ListViewItem>
+            <Badge theme="main">5</Badge>
+          </ListViewItem>
+          <ListViewItem>
+            <Badge theme="positive">5</Badge>
+          </ListViewItem>
+          <ListViewItem>
+            <Badge theme="negative">5</Badge>
+          </ListViewItem>
+          <ListViewItem>
+            <Badge theme="main" inverse="true">5</Badge>
+          </ListViewItem>
+          <ListViewItem>
+            <Badge theme="positive" inverse="true">5</Badge>
+          </ListViewItem>
+          <ListViewItem>
+            <Badge theme="negative" inverse="true">5</Badge>
+          </ListViewItem>
+        </ListView>
+      </div>
     );
   }
 }
