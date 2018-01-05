7afaf6e9904b08544ad5e751dea0c1cf74f265bd
Matthew Daly <matthewbdaly@gmail.com>
2018-01-05 15:10:38 +0000

    Added type checking for the button component

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -1,8 +1,21 @@
+// @flow
 import React, { Component } from 'react';
+import type { Node } from 'react';
 import './Button.scss';
 
-class Button extends Component {
-  constructor(props) {
+type Props = {
+  theme: string,
+  inverse: boolean,
+  block: boolean,
+  children?: Node
+};
+
+type State = {
+  theme: string
+}
+
+class Button extends Component<Props, State> {
+  constructor(props: Props) {
     super(props);
     let theme = this.props.theme;
     if (this.props.inverse) {
