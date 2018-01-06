d404c0080d02d827f48da99a97774c837e09250d
Matthew Daly <matthewbdaly@gmail.com>
2018-01-06 18:28:49 +0000

    Amended bar component

--- src/App.js
+++ src/App.js
@@ -35,6 +35,12 @@ class BarPage extends Component {
     return (
       <div>
         <Bar>This is a default bar</Bar>
+        <Bar theme="main">This is a main bar</Bar>
+        <Bar theme="positive">This is a positive bar</Bar>
+        <Bar theme="negative">This is a negative bar</Bar>
+        <Bar theme="main" inverse="true">This is an inverse main bar</Bar>
+        <Bar theme="positive" inverse="true">This is an inverse positive bar</Bar>
+        <Bar theme="negative" inverse="true">This is an inverse negative bar</Bar>
       </div>
     );
   }
--- src/Components/Bar.js
+++ src/Components/Bar.js
@@ -4,13 +4,34 @@ import type { Node } from 'react';
 import './Bar.scss';
 
 type Props = {
+  theme: string,
   children?: Node
 };
 
-class Bar extends Component<Props> {
+type State = {
+  theme: string
+}
+
+class Bar extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    let theme = "bar";
+    if (this.props.theme) {
+      theme += " " + this.props.theme;
+    }
+    if (this.props.inverse) {
+      theme += " inverse";
+    }
+    if (this.props.block) {
+      theme += " block";
+    }
+    this.state = {
+      theme: theme
+    };
+  }
   render() {
     return (
-      <div className="bar">
+      <div className={this.state.theme}>
         {this.props.children}
       </div>
     );
