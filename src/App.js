74eb6740b97810f51af8464900ed6e6665b90dfb
Matthew Daly <matthewbdaly@gmail.com>
2018-01-08 13:36:28 +0000

    Amended sidebar toggle button

--- src/App.js
+++ src/App.js
@@ -72,8 +72,8 @@ class App extends Component<Props, State> {
         <div className="App">
           <Sidebar links={sidebarlinks} active={this.state.sidebarActive}>
           </Sidebar>
-          <Bar theme="main">
-            <Button theme="main" clickHandler={this.toggleSidebar.bind(this)}>
+          <Bar>
+            <Button theme="positive" rounded="true" inverse="true" clickHandler={this.toggleSidebar.bind(this)}>
               <i className="fas fa-list"></i>
             </Button>
           </Bar>
