a531e73af2da454b4cfc877fc23d40656dbcbf35
Matthew Daly <matthewbdaly@gmail.com>
2018-05-13 17:15:18 +0100

    Amended home page text

--- src/Pages/IndexPage.js
+++ src/Pages/IndexPage.js
@@ -4,7 +4,7 @@ class IndexPage extends Component {
   render() {
     return (
       <div>
-        <p>A React component library for use with Phonegap applications.</p>
+        <p>A React component library, intended primarily for use with Phonegap applications.</p>
         <p>These components are thoroughly tested, including snapshot tests, and are thoroughly typed using Flow.</p>
       </div>
     );
